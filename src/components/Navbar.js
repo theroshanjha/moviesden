7063d3b95f67d4250220010778064f9424c1bba1
theroshanjha <roshanjha521@gmail.com>
2025-03-26 16:15:39 +0530

    fdih

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar(props) {
   return (
     <nav
       className={`navbar navbar-expand-lg navbar -${props.mode} bg-${props.mode}`}
-      // okokoko
+      // okokokodskhbfdihabdhwibhidwvb
     >
       <div className="container-fluid">
         <Link

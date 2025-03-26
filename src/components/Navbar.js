9ae496486937dcfc56d6cb5ab85f05c4b611a633
theroshanjha <roshanjha521@gmail.com>
2025-03-26 16:13:17 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,7 @@ export default function Navbar(props) {
   return (
     <nav
       className={`navbar navbar-expand-lg navbar -${props.mode} bg-${props.mode}`}
+      // okokoko
     >
       <div className="container-fluid">
         <Link
